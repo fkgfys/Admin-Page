d08c8ef58d50a19142482e636e4306d42053b44c
lingkai810975 <97112572+fkgfys@users.noreply.github.com>
2023-12-08 14:04:35 -0500

    Update Admin.js

--- Admin.js
+++ Admin.js
@@ -1,80 +1,47 @@
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { Link, useMatch, useResolvedPath } from 'react-router-dom';
+import React, { useEffect } from 'react';
 
-function Admin() {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-  const navigate = useNavigate(); // Initialize the history object
-  // const [inputs, setInputs] = useState({})
+const Navbar = ({ isLoggedIn }) => {
+  useEffect(() => {
+    console.log('isLoggedIn changed:', isLoggedIn);
+  }, [isLoggedIn]);
+    return (
+      <nav className="navbar">
+        <div>
+          <Link id="logo" to="/">
+            <h1>BioSMART</h1>
+            <h2><span className="black">Bio</span>psy adequacy assessment on <span className="black">smart</span>phones</h2>
+          </Link>
+        </div>
 
-  // const handleChange= (event) => {
-  //   const name = event.target.name;
-  //   const value = event.target.value;
+        <ul className="links">
+          <CustomLink to="/">Home</CustomLink>
+          {isLoggedIn ? (
 
-  //   setInputs(values => ({...values, [name]: value}));
+            <CustomLink to="/demo">Demo</CustomLink>
 
-  // }
-  function getCookie(name) {
-    const cookieValue = document.cookie
-      .split('; ')
-      .find(row => row.startsWith(name + '='))
-      ?.split('=')[1]; // Get the value of the cookie with the specified name
-    return cookieValue ? decodeURIComponent(cookieValue) : null;
-  }
+        ) : (
 
-  const handleAdmin = () => {
-    // Check if the entered credentials are correct (e.g., hard-coded values)
-    const csrftoken = getCookie('csrftoken'); // Fetch the CSRF token from the cookie
-    const headers = {
-      'X-CSRFToken': csrftoken, // Include the CSRF token in the headers
-    };
+            <CustomLink to="/login">Login</CustomLink>
 
-    const uploadData = new FormData();
-    uploadData.append('username', username);
-    uploadData.append('password', password);
-    fetch('http://localhost:8000/api/create_account/', {
-      method: 'POST',
-      headers: headers, // Include the headers with the CSRF token
-      body: uploadData,
-      credentials: 'include'
-    })
-      .then((res) => {
-        console.log(res);
-      })
-  };
-
-  // const handleSubmit = (event) => {
-  //   event.preventDefault();
-
-  //   axios.post('http://localhost:8888/api/user/save', inputs);
-  //   console.log(inputs);
-  // }
-  // in <form> tag: onSubmit={handleSubmit}
-
-  return (
-    <div className="admin">
-      <div>
-
-        <div id = "title_admin">Admin</div>
-
-          <label class="custom-field one">
-            <input type="text" placeholder=" " name = "username" value={username} onChange={(e) => setUsername(e.target.value)}/>
-            <span class="placeholder">Username</span>
-          </label>
-
-          <br/>
-
-          <label class="custom-field one">
-            <input type="text" placeholder="&nbsp;" name = "password" value={password} onChange={(e) => setPassword(e.target.value)}/>
-            <span class="placeholder">Password</span>
-          </label>
-
-          <br/>
-
-          <button id = "button" onClick={handleAdmin}>Create Account</button>
-    
-  </div>
-    </div>
-  );
+        )}
+            <CustomLink to="/about">About</CustomLink>
+            <CustomLink to="/contact">Contact</CustomLink>
+        </ul>
+      </nav>  
+    );
 }
-export default Admin;
+
+function CustomLink({ to, children, ...props}) {
+  const resolvedPath = useResolvedPath(to)
+  const isActive = useMatch({ path: resolvedPath.pathname, end: true})
+  return(
+    <li className={isActive ? "active" : ""}>
+      <Link to={to} {...props}>
+        {children}
+      </Link>
+    </li>
+  )
+}
+ 
+export default Navbar;
